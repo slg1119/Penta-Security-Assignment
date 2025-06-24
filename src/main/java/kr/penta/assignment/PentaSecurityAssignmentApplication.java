7ff9756a58b0b6a6fdb6ad935eb45c4d5b459eb4
RokHyung Son <fhrgud1264@gmail.com>
2025-06-25 07:11:29 +0900

    first commit

---
+++ src/main/java/kr/penta/assignment/PentaSecurityAssignmentApplication.java
@@ -0,0 +1,13 @@
+package kr.penta.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PentaSecurityAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PentaSecurityAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/penta/assignment/PentaSecurityAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.penta.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PentaSecurityAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
